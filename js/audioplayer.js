1960fd8f9091e3ab7fbe23accb894983d8791727
Alan Rodrigues <alancrato@hotmail.com>
2017-11-14 15:33:30 -0200

    Primeiro Commit Iframe da Plus

---
+++ js/audioplayer.js
@@ -0,0 +1,116 @@
+var music = document.getElementById('music'); // id for audio element
+var duration = music.duration; // Duration of audio clip, calculated here for embedding purposes
+var pButton = document.getElementById('pButton'); // play button
+var pButton2 = document.getElementById('pButton2'); // play button
+var playhead = document.getElementById('playhead'); // playhead
+var timeline = document.getElementById('timeline'); // timeline
+
+// timeline width adjusted for playhead
+var timelineWidth = timeline.offsetWidth - playhead.offsetWidth;
+
+// play button event listenter
+pButton.addEventListener("click", play);
+pButton2.addEventListener("click", play);
+
+// timeupdate event listener
+music.addEventListener("timeupdate", timeUpdate, false);
+
+// makes timeline clickable
+timeline.addEventListener("click", function(event) {
+    moveplayhead(event);
+    music.currentTime = duration * clickPercent(event);
+}, false);
+
+// returns click as decimal (.77) of the total timelineWidth
+function clickPercent(event) {
+    return (event.clientX - getPosition(timeline)) / timelineWidth;
+}
+
+// makes playhead draggable
+playhead.addEventListener('mousedown', mouseDown, false);
+window.addEventListener('mouseup', mouseUp, false);
+
+// Boolean value so that audio position is updated only when the playhead is released
+var onplayhead = false;
+
+// mouseDown EventListener
+function mouseDown() {
+    onplayhead = true;
+    window.addEventListener('mousemove', moveplayhead, true);
+    music.removeEventListener('timeupdate', timeUpdate, false);
+}
+
+// mouseUp EventListener
+// getting input from all mouse clicks
+function mouseUp(event) {
+    if (onplayhead == true) {
+        moveplayhead(event);
+        window.removeEventListener('mousemove', moveplayhead, true);
+        // change current time
+        music.currentTime = duration * clickPercent(event);
+        music.addEventListener('timeupdate', timeUpdate, false);
+    }
+    onplayhead = false;
+}
+// mousemove EventListener
+// Moves playhead as user drags
+function moveplayhead(event) {
+    var newMargLeft = event.clientX - getPosition(timeline);
+
+    if (newMargLeft >= 0 && newMargLeft <= timelineWidth) {
+        playhead.style.marginLeft = newMargLeft + "px";
+    }
+    if (newMargLeft < 0) {
+        playhead.style.marginLeft = "0px";
+    }
+    if (newMargLeft > timelineWidth) {
+        playhead.style.marginLeft = timelineWidth + "px";
+    }
+}
+
+// timeUpdate
+// Synchronizes playhead position with current point in audio
+function timeUpdate() {
+    var playPercent = timelineWidth * (music.currentTime / duration);
+    playhead.style.marginLeft = playPercent + "px";
+    if (music.currentTime == duration) {
+        pButton.className = "";
+        pButton.className = "play";
+        pButton2.className = "";
+        pButton2.className = "play";
+    }
+}
+
+//Play and Pause
+function play() {
+    // start music
+    if (music.paused) {
+        music.play();
+        SW.start();
+        // remove play, add pause
+        pButton.className = "";
+        pButton.className = "pause";
+        pButton2.className = "";
+        pButton2.className = "pause";
+    } else { // pause music
+        music.pause();
+        SW.phase = 0;
+        SW.run = false;
+        // remove pause, add play
+        pButton.className = "";
+        pButton.className = "play";
+        pButton2.className = "";
+        pButton2.className = "play";
+    }
+}
+
+// Gets audio file duration
+music.addEventListener("canplaythrough", function() {
+    duration = music.duration;
+}, false);
+
+// getPosition
+// Returns elements left position relative to top-left of viewport
+function getPosition(el) {
+    return el.getBoundingClientRect().left;
+}
---
+++ sobral.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+
+    <title>Plus Fm - Aqui é legal demais!</title>
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/style.css" />
+    <link rel="shortcut icon" href="img/favicon.png"/>
+
+</head>
+
+<body>
+
+<section>
+
+    <!--Oculto apenas para mobile-->
+    <div id="player_original">
+        <audio id="music" src="http://198.24.156.115:9306/;" preload="true" autoplay controls></audio>
+    </div>
+
+    <div id="audioplayer">
+
+        <div class="container">
+            <div class="row">
+
+                <div class="col-md-2 hidden-sm hidden-xs">
+                    <div id="buuton">
+                        <button id="pButton" class="pause"></button>
+                    </div>
+                </div>
+
+                <div class="col-sm-2 col-xs-2 visible-sm visible-xs">
+                    <div id="buuton2">
+                        <button id="pButton2" class="play"></button>
+                    </div>
+                </div>
+
+
+                <div class="col-md-10 col-sm-10 col-xs-10">
+                    <div class="squares">
+                        <ul>
+                            <li><a href="index.php">Rede</a></li>
+                            <li><a href="aracati.php">Aracati</a></li>
+                            <li><a href="crateus.php">Crateús</a></li>
+                            <li><a href="index.php">Iguatu/Cariús</a></li>
+                            <li class="hidden-xs"><a href="paraipaba.php">Paraipaba</a></li>
+                            <li class="hidden-xs hidden-sm"><a href="redencao.php">Redenção</a></li>
+                            <li class="hidden-xs hidden-sm"><a href="santa-quiteria.php">St° Quitéria</a></li>
+                            <li class="hidden-xs hidden-sm"><a href="sobral.php" style="color: #d25246">Sobral</a></li>
+                            <li class="dropdown">
+                                <a href="javascript:void(null)" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="caret"></span></a>
+                                <ul class="dropdown-menu">
+                                    <li class="visible-xs"><a href="paraipaba.php">Paraipaba</a></li>
+                                    <li class="visible-xs visible-sm"><a href="redencao.php">Redenção</a></li>
+                                    <li class="visible-xs visible-sm"><a href="santa-quiteria.php">St° Quitéria</a></li>
+                                    <li class="visible-xs visible-sm"><a href="sobral.php">Sobral</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+
+    <div id="timeline">
+        <div id="playhead">
+        </div>
+    </div>
+
+</section>
+
+<iframe frameborder="0" width="100%" height="100%" src="http://localhost/draco/plusfm/novo/">
+    <p>Seu navegador não suporta iframes.</p>
+</iframe>
+
+<!-- Js -->
+<script src="js/jquery.js"></script>
+<script src="js/bootstrap.js"></script>
+
+<script src="js/audioplayer.js"></script>
+<script src="js/siriwave.js"></script>
+<script>
+    var SW = new SiriWave({
+        height: 100,
+        speed: 0.1,
+        amplitude: 1,
+        container: document.getElementById('siric'),
+        autostart: true,
+    });
+</script>
+
+</body>
+</html>
